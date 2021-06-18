7983c5fd80ac7f5db0bdf84e0d3894bf2d84fffd
Saanvi-Srivastava <76878363+Saanvi-Srivastava@users.noreply.github.com>
2021-06-17 22:05:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+    song = loadSound(music.mp3);
+}
+
+function play()
+{
+    song.play();
+    song.setvolume(1);
+    song.rate(1);
+}
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized!");
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWrist X = " + leftWristX + "leftWrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWrist X = " + rightWristX + "rightWrist Y = " + rightWristY);
+    }
+}
